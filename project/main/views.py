c97d01f8a7cdc22c247476b0e0597f48a9d82f29
megakirill <mumu.moscow@gmail.com>
2022-03-02 16:34:26 +0300

    Добавил index.html

---
+++ project/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def get_menu_context():
+    return [
+        {'url_name': 'index'},
+        {'url_name': 'time', 'name': 'Добавить данные', 'name1': 'Просмотрр карты'},
+    ]
+def index_page(request):
+    context = {
+        'pagename': 'Главная',
+        'author': 'балбесы',
+        'menu': get_menu_context(),
+    }
+    return render(request, 'pages/index.html', context)
