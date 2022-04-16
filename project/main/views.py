23babab24fae24c7134491a10c3cfbce4ebcb810
megakirill <mumu.moscow@gmail.com>
2022-04-16 16:57:29 +0300

    Пофиксил несколько багов

--- project/main/views.py
+++ project/main/views.py
@@ -6,16 +6,19 @@ def get_menu_context():
         {'url_name': 'index'},
         {'url_name': 'time', 'name': 'Добавить данные', 'name1': 'Просмотр карты'},
     ]
+
 def index_page(request):
     context = {
         'menu': get_menu_context(),
     }
     return render(request, 'pages/index.html', context)
+
 def add_page(request):
     context = {
         'menu': get_menu_context(),
     }
     return render(request, 'pages/add.page.html', context)
+
 def map_page(request):
     context = {
         'menu': get_menu_context(),
